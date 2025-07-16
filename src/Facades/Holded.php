6039489c4d2f946d8ac356211810ba9644fd1fb7
Xavi Cabot <xavi@unancor.com>
2025-07-16 13:32:17 +0200

    Update Holded.php

--- src/Facades/Holded.php
+++ src/Facades/Holded.php
@@ -3,19 +3,18 @@
 namespace XaviCabot\Laravel\Holded\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use XaviCabot\Laravel\Holded\Modules\DocumentModule;
+use XaviCabot\Laravel\Holded\Modules\ContactModule;
 
 /**
- * @method static array listContacts(int $page = 1)
- * @method static array getContact(string $id)
- * @method static array createContact(array $data)
- * @method static array updateContact(string $id, array $data)
- * @method static array createDocument(string $type, array $data)
+ * @method static DocumentModule document()
+ * @method static ContactModule contact()
  */
 
 class Holded extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
-        return 'holded';
+        return \XaviCabot\Laravel\Holded\Holded::class;
     }
 }
